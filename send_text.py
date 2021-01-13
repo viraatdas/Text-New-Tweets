ceae8f8b80ebb30e9a6bb9e3ddd417d18b746641
Viraat <viraat.laldas@gmail.com>
2021-01-13 04:09:21 -0500

    testing on ec2 instance

--- send_text.py
+++ send_text.py
@@ -1,6 +1,7 @@
 from src.smtp_text import smtp_text
 from src.twitter_api import twitter_api
 from config import *
+import time
 
 phone_numbers = []
 with open('phone_numbers.txt') as f:
@@ -12,8 +13,9 @@ twitter_account = twitter_api(API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKE
 
 tweet_text, time_in_seconds = twitter_account.get_latest_tweet()
 
-if twitter_account.set_tweet_time_if_latest(time_in_seconds):
-    for phone_number in phone_numbers:
-        text_smtp.send_message(phone_number, tweet_text)
-
-
+while True:
+    if twitter_account.set_tweet_time_if_latest(time_in_seconds):
+        for phone_number in phone_numbers:
+            text_smtp.send_message(phone_number, tweet_text)
+    print("Successfully sent updated tweet")
+    time.sleep(10)
